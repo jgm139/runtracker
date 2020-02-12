463c5072ec98c8993db34a9264da28215c00bafa
Julia García Martínez <jgm139@alu.ua.es>
2020-02-12 19:15:59 +0100

    Inicio del proyecto

---
+++ RunTracker/RunTracker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RunTracker
+//
+//  Created by Julia García Martínez on 12/02/2020.
+//  Copyright © 2020 Julia García Martínez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
